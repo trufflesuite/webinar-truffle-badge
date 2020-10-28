1649585fb65f63d243dc7d9b4c5a06b8a09d2943
Kevin Bluer <kevin@bluer.com>
2020-10-28 13:32:46 -0500

    comment

--- contracts/TruffleBadge.sol
+++ contracts/TruffleBadge.sol
@@ -4,6 +4,8 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TruffleBadge is ERC721 {
 
+    /// @notice Vanilla instance of OZ's ERC721 implementation
+    /// @dev However there's no actual means of minting any badges after deployment
     constructor() ERC721("TruffleBadge", "TRFL") public {
     }
 }
