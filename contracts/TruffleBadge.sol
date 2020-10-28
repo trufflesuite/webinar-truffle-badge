1c7250b8ededcfc41881e6a968b8f5217500480f
Kevin Bluer <kevin@bluer.com>
2020-10-28 12:52:04 -0500

    tidying

--- contracts/TruffleBadge.sol
+++ contracts/TruffleBadge.sol
@@ -1,19 +1,9 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract TruffleBadge is ERC721 {
 
     constructor() ERC721("TruffleBadge", "TRFL") public {
-    }
-    
-    function awardBadge(address attendee)
-        public
-        returns (uint256)
-    {
-        uint badgeId = 888;
-        _mint(attendee, badgeId);
-        return badgeId;
     }
 }
