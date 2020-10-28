36d6abd80f5f4e6c9c3ef273ba5cadeedef699aa
Kevin Bluer <kevin@bluer.com>
2020-10-28 11:13:00 -0500

    mint only

--- contracts/TruffleBadge.sol
+++ contracts/TruffleBadge.sol
@@ -4,21 +4,15 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract TruffleBadge is ERC721 {
-    using Counters for Counters.Counter;
-    Counters.Counter private _badgeIds;
 
     constructor() ERC721("TruffleBadge", "TRFL") public {
     }
     
-    function awardBadge(address attendee, string memory attendenceBadgeURI)
+    function awardBadge(address attendee)
         public
         returns (uint256)
     {
-        _badgeIds.increment();
-
-        uint256 newBadgeId = _badgeIds.current();
-        _mint(attendee, newBadgeId);
-        _setTokenURI(newBadgeId, attendenceBadgeURI);
+        _mint(attendee, 888);
 
         return newBadgeId;
     }
