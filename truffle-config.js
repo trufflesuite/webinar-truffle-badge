24bee86c95f9bdea6a24fc2bf8edf3bd51852ea1
Kevin Bluer <kevin@bluer.com>
2020-11-19 17:25:53 -0600

    adding rinkeby / hdwalletprovider as a dep

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const mnemonic = "***";
+
 module.exports = {
 
   networks: {
@@ -9,6 +12,11 @@ module.exports = {
     sandbox: {
       url: `https://sandbox.truffleteams.com/59abc1ee-2123-4491-8507-4408ee2274db`,
       network_id: "*",
+      gas: 5500000
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/119d469b3cc3488cbcee85eb4309cd7b`),
+      network_id: "4",
       gas: 5500000
     }
   },
