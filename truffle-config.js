aa4ef7f32b9706f914808f85ce8353bfb39c4f6a
Kevin Bluer <kevin@bluer.com>
2020-11-20 11:09:40 -0600

    adding rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const mnemonic = "***";
+
 module.exports = {
 
   networks: {
@@ -7,13 +10,13 @@ module.exports = {
      network_id: "*",
     },
     sandbox: {
-      url: `ws://marshmallow:8002/3ce85486-1f0f-4010-8576-9527eb9b866d`,
+      url: `https://sandbox.truffleteams.com/59abc1ee-2123-4491-8507-4408ee2274db`,
       network_id: "*",
       gas: 5500000
     },
-    remote: {
-      url: `https://sandbox.truffleteams.com/2cc01156-56d7-44b5-80cd-7f822f2ef044`,
-      network_id: "*",
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/119d469b3cc3488cbcee85eb4309cd7b`),
+      network_id: "4",
       gas: 5500000
     }
   },
