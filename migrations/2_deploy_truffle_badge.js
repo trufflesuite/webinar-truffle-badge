9f034f9ddd2362caa1b7dc8b9fba8d6d15709fae
Kevin Bluer <kevin@bluer.com>
2020-12-09 15:59:26 -0600

    opengsn

--- migrations/2_deploy_truffle_badge.js
+++ migrations/2_deploy_truffle_badge.js
@@ -1,7 +1,14 @@
-const TruffleBadge = artifacts.require("TruffleBadge");
+const TruffleBadge = artifacts.require('TruffleBadge')
 
-module.exports = function(deployer) {
-   
-  return deployer
-    .then(() => deployer.deploy(TruffleBadge));
-};
+module.exports = async function deployFunc (deployer, network) {
+
+  const forwarder = await require('../build/gsn/Forwarder.json').address;
+
+  if (!forwarder) {
+    throw new Error('No valid forwarder for network ' + network);
+  }
+
+  console.log(`Using forwarder: ${forwarder}`);
+  await deployer.deploy(TruffleBadge, forwarder, { gas: 2.6e6 });
+  console.log(`Done!`)
+}
