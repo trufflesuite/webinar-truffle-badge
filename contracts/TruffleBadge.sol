07fdb800703c819358a5259ff682be77b2561387
Kevin Bluer <kevin@bluer.com>
2020-12-09 17:36:07 -0600

    opengsn tweaks

--- contracts/TruffleBadge.sol
+++ contracts/TruffleBadge.sol
@@ -1,10 +1,33 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@opengsn/gsn/contracts/BaseRelayRecipient.sol";
+import "@opengsn/gsn/contracts/interfaces/IKnowForwarderAddress.sol";
 
-contract TruffleBadge is ERC1155 {
+contract TruffleBadge is ERC1155, BaseRelayRecipient, IKnowForwarderAddress {
+    address public deployer;
 
-    constructor() ERC1155("https://badges.trufflesuite.com/{id}.json") public {
+    constructor(address forwarder) ERC1155("https://badges.trufflesuite.com/{id}.json") public {
+        trustedForwarder = forwarder;
+        deployer = msg.sender;
+    }
+
+    function getTrustedForwarder() public override view returns(address) {
+        return trustedForwarder;
+    }
+
+    function setTrustedForwarder(address forwarder) public {
+        require(_msgSender() == deployer, "Only deployer can update it");
+
+        trustedForwarder = forwarder;
+    }
+
+    function _msgSender() internal view override(Context, BaseRelayRecipient) returns (address payable) {
+        return BaseRelayRecipient._msgSender();
+    }
+
+    function _msgData() internal view override(Context, BaseRelayRecipient) returns (bytes memory) {
+        return BaseRelayRecipient._msgData();
     }
     
     /// @notice Ability to add award a badge (via _mint) to a webinar attendee via their EOA 
@@ -20,11 +43,19 @@ contract TruffleBadge is ERC1155 {
         _mint(attendee, badgeType, 1, '');
     }
 
+    function claimBadgeNoGas() public {
+        _mint(_msgSender(), 888, 1, '');
+    }
+
     function awardBadgeBatch(address attendee, uint256[] memory badgeTypes, uint256[] memory amounts)
         public
         returns (uint256)
     {
         _mintBatch(attendee, badgeTypes, amounts, '');
+    }
+
+    function versionRecipient() external virtual view override returns (string memory) {
+        return "1.0";
     }
 
 }
--- migrations/2_deploy_truffle_badge.js
+++ migrations/2_deploy_truffle_badge.js
@@ -1,14 +1,6 @@
 const TruffleBadge = artifacts.require('TruffleBadge')
 
 module.exports = async function deployFunc (deployer, network) {
-
   const forwarder = await require('../build/gsn/Forwarder.json').address;
-
-  if (!forwarder) {
-    throw new Error('No valid forwarder for network ' + network);
-  }
-
-  console.log(`Using forwarder: ${forwarder}`);
-  await deployer.deploy(TruffleBadge, forwarder, { gas: 2.6e6 });
-  console.log(`Done!`)
+  await deployer.deploy(TruffleBadge, forwarder);
 }
